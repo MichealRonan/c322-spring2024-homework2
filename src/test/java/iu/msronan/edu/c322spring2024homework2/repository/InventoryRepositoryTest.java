bf69ab915c8526b8d15664cdf4c14de1b01eea5b
MichealRonan <132925162+MichealRonan@users.noreply.github.com>
2024-02-07 21:24:33 -0600

    Homework 3

--- src/test/java/iu/msronan/edu/c322spring2024homework2/repository/InventoryRepositoryTest.java
+++ src/test/java/iu/msronan/edu/c322spring2024homework2/repository/InventoryRepositoryTest.java
@@ -14,17 +14,17 @@ class InventoryRepositoryTest {
     void addGuitar() {
         repository = new InventoryRepository();
 
-        Guitar guitar = new Guitar("111", 1000.0, "Ford", "Model1", "Type1", "Wood1", "Wood2");
+        Guitar guitar = new Guitar("111", 1000.0, Guitar.Builder.FENDER, "Model1", Guitar.Type.ACOUSTIC, Guitar.Wood.BRAZILIAN_ROSEWOOD, Guitar.Wood.CEDAR);
         repository.addGuitar(guitar);
 
         Guitar found = repository.getGuitar("111");
         assertNotNull(found);
-        assertEquals("Ford", found.getBuilder());
+        assertEquals(Guitar.Builder.FENDER, found.getBuilder());
 
         repository = new InventoryRepository();
 
         int initialSize = repository.search(new Guitar(null, 0, null, null, null, null, null)).size();
-        repository.addGuitar(new Guitar("232", 1500.0, "Apple", "Model2", "Type2", "Wood3", "Wood4"));
+        repository.addGuitar(new Guitar("232", 1500.0, Guitar.Builder.COLLINGS, "Model2", Guitar.Type.ELECTRIC, Guitar.Wood.COCOBOLO, Guitar.Wood.ALDER));
 
         int newSize = repository.search(new Guitar(null, 0, null, null, null, null, null)).size();
         assertEquals(initialSize + 1, newSize);
@@ -34,23 +34,21 @@ class InventoryRepositoryTest {
     void getGuitar() {
         repository = new InventoryRepository();
 
-        repository.addGuitar(new Guitar("423", 2000.0, "Adidas", "Model3", "Type3", "Wood5", "Wood6"));
+        repository.addGuitar(new Guitar("423", 2000.0, Guitar.Builder.PRS, "Model3", Guitar.Type.ELECTRIC, Guitar.Wood.MAHOGANY, Guitar.Wood.INDIAN_ROSEWOOD));
         Guitar guitar = repository.getGuitar("423");
 
         assertNotNull(guitar);
-        assertEquals("Adidas", guitar.getBuilder());
+        assertEquals(Guitar.Builder.PRS, guitar.getBuilder());
         assertEquals(2000.0, guitar.getPrice());
     }
 
     @Test
     void search() {
-        repository = new InventoryRepository();
+        repository.addGuitar(new Guitar("453", 2500.0, Guitar.Builder.RYAN, "Model4", Guitar.Type.ELECTRIC, Guitar.Wood.COCOBOLO, Guitar.Wood.MAPLE));
+        repository.addGuitar(new Guitar("765", 3000.0, Guitar.Builder.RYAN, "Model5", Guitar.Type.ACOUSTIC, Guitar.Wood.COCOBOLO, Guitar.Wood.SITKA));
 
-        repository.addGuitar(new Guitar("453", 2500.0, "Tesla", "Model4", "Type4", "Wood7", "Wood8"));
-        repository.addGuitar(new Guitar("765", 3000.0, "Tesla", "Model5", "Type5", "Wood7", "Wood9"));
-
-        List<Guitar> found = repository.search(new Guitar(null, 0, "Tesla", null, null, "Wood7", null));
+        List<Guitar> found = repository.search(new Guitar(null, 0, Guitar.Builder.RYAN, null, null, Guitar.Wood.COCOBOLO, null));
         assertEquals(2, found.size());
-        assertTrue(found.stream().allMatch(guitar -> guitar.getBuilder().equals("Tesla") && guitar.getBackWood().equals("Wood7")));
+        assertTrue(found.stream().allMatch(guitar -> false));
     }
 }
